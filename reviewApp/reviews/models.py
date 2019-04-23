51bb4ce38dc8c2225b629dba89639970a10ba630
Lee_Catling <b5020404@my.shu.ac.uk>
2019-04-23 16:38:22 +0100

    Improved the appearance of the website using cripsy and also created the registration for users

--- reviewApp/reviews/models.py
+++ reviewApp/reviews/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+class Issue(models.Model):
+	type=models.CharField(max_length=100)
+	room=models.CharField(max_length=100)
+	details=models.TextField()
+	date_submitted=models.DateTimeField(default=timezone.now)
+	author=models.ForeignKey(User, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.type
 
 # Create your models here.
--- reviewApp/reviews/urls.py
+++ reviewApp/reviews/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
+from users import views as user_views
 
 urlpatterns = [
-	path('', views.home, name='reviewApp-home'),
-	path('about/', views.about, name='reviewApp-about'),
-	path('contact/', views.contact, name='reviewApp-contact'),
-	path('product/', views.product, name='reviewApp-product'),
-
+	path('', views.home, name='reviews-home'),
+	path('about/', views.about, name='reviews-about'),
+	path('contact/', views.contact, name='reviews-contact'),
+	path('product/', views.product, name='reviews-product'),
+	path('register/', user_views.register, name='register'),
 ]
